8706de7d52b560877065a7efd56442b59ba5ee40
Alexey Ivanov <aivanov@mobiledimension.ru>
2019-02-27 15:30:40 +0300

    init

---
+++ DemoTwoDirectionalCollectionViewLayout/Colors/ColorSequence.swift
@@ -0,0 +1,41 @@
+//
+//  ColorSequence.swift
+//  Sequences
+//
+//  License
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import UIKit
+
+
+extension UIColor {
+    
+    /**
+     Sequence that generates the `next` color using a Sine wave.
+     - returns: `AnySequence<UIColor>`
+     - parameter frequency: how fast the sine wave oscillates
+     - parameter phase1: color phase in radians for red
+     - parameter phase2: color phase in radians for green
+     - parameter phase3: color phase in radians for blue
+     - parameter amplitude: how high and low the sine wave reaches.
+     - parameter center: the center position of the sine wave.
+     - parameter repeat: if `true` the sequence will continue infinitely; defaults to false
+     */
+    public static func rainbowSequence(frequency:   CGFloat = Default.frequency,
+                                       phase1:      CGFloat = Default.phase1,
+                                       phase2:      CGFloat = Default.phase2,
+                                       phase3:      CGFloat = Default.phase3,
+                                       amplitude:   CGFloat = Default.amplitude,
+                                       center:      CGFloat = Default.center,
+                                       repeat:      Bool    = false) -> AnySequence<UIColor> {
+        
+        return AnySequence(rainbowIterator(frequency:  frequency,
+                                           phase1:     phase1,
+                                           phase2:     phase2,
+                                           phase3:     phase3,
+                                           amplitude:  amplitude,
+                                           center:     center,
+                                           repeat:     `repeat`))
+    }
+}
--- DemoTwoDirectionalCollectionViewLayout/ViewController.swift
+++ DemoTwoDirectionalCollectionViewLayout/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var colors: [[UIColor]] = {
+        let colors = stride(from: 360, through: 0, by: -30)
+            .map { (i) -> AnySequence<UIColor> in
+                let phase = CGFloat(i) * CGFloat.pi / 180
+                return UIColor.rainbowSequence(phase1: 0, phase2: phase, phase3: phase * 2)
+        }
+        return colors.map {Array($0)}
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,3 +27,23 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UICollectionViewDataSource {
+    
+    //number of sections == number of rows
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return colors.count
+    }
+    
+    //number of items == number of columns
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return colors[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath)
+        
+        let color = colors[indexPath.section][indexPath.row]
+        cell.backgroundColor = color
+        return cell
+    }
+}
