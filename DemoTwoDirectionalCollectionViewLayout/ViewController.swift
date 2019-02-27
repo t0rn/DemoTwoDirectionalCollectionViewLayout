264546415e51ac0c26ac27deffadd2157118ac37
Alexey Ivanov <aivanov@mobiledimension.ru>
2019-02-27 14:39:12 +0300

    Initial Commit

---
+++ DemoTwoDirectionalCollectionViewLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoTwoDirectionalCollectionViewLayout
+//
+//  Created by Алексей Иванов on 27/02/2019.
+//  Copyright © 2019 Alexey Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
